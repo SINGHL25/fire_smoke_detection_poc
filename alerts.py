6b89b4c60fe1f6d29d73fb4474b17fe9b974a0c9
Akhilesh singh <akhi.singh1989@gmail.com>
2025-07-21 20:12:51 +1000

    Update alerts.py

--- alerts.py
+++ alerts.py
@@ -1,1 +1,13 @@
+import smtplib
+from email.mime.text import MIMEText
+
+def send_email_alert():
+    msg = MIMEText("🔥 ALERT: Fire/Smoke Detected by SmartSafeAI")
+    msg["Subject"] = "FIRE ALERT"
+    msg["From"] = "your-email@gmail.com"
+    msg["To"] = "recipient@gmail.com"
+
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
+        server.login("your-email@gmail.com", "your-app-password")
+        server.send_message(msg)
 
