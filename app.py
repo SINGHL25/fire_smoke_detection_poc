cd9fc07e06a5c087eb55ad499790defe256b5ef9
Akhilesh singh <akhi.singh1989@gmail.com>
2025-07-21 20:10:49 +1000

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,22 @@
+from fire_model import predict_fire
+from alerts import send_email_alert
+from logger import log_event_sqlite  # or log_event_firebase
+
+if run:
+    st.success("Camera running with CNN detection...")
+    while run:
+        ret, frame = cap.read()
+        if not ret:
+            st.error("Frame not captured.")
+            break
+
+        fire_detected = predict_fire(frame)
+
+        if fire_detected:
+            st.warning("🔥 Fire/Smoke Detected (CNN)")
+            send_email_alert()
+            log_event_sqlite("FIRE DETECTED")  # or log_event_firebase()
+
+        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        FRAME_WINDOW.image(frame_rgb)
 
